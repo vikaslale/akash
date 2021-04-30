0f4de45254275f40c418f0fec353f350dc7efedc
vikas lale <vikaslalevpl@gmail.com>
2021-04-30 14:35:18 +0530

    set commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Student from'./student';
+export default class App extends Component {
+   constructor(props){
+     super(props);
+     console.log("app - Constructor called");
+     console.log(props);
+     this.state ={
+       roll: '101'
+     };
+   }
+   static getDerivedStateFromProps(props,state){
+     console.log("aApp-get Derived state from Props");
+     console.log(props, state);
+     return null;
+   }
+  render() {
+    return (
+      <div>
+          <Student name="Rahul"/>
+      </div>
+    )
+  }
 }
-
-export default App;
